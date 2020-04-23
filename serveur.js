ee1ee9c6c35ec5bacedddc287b53c7523afe1a09
Todeno <todenoo@gmail.com>
2020-04-23 10:37:36 +0200

    Création input code secret et ajout système comparaison code donnée et code attendu côté serveur (retourne ou non la vidéo en fonction)

--- serveur.js
+++ serveur.js
@@ -1,13 +1,29 @@
 const express = require('express'); // Importation d'express
 const path = require('path'); // Importation de path
+const fs = require('fs'); // Importation de fs (File System)
 
 const app = express(); // Initialisation de app 
 const PORT = process.env.PORT || 5000; // Initialisation du port avec le fichier env ou 5000 par défaut.
 
 // Récupération de la vidéo via la route /video
-app.get('/videos/:nom', (req, res) => {
-    res.sendFile(path.join(__dirname, 'videos', req.params.nom+'.mp4'));
-});
+app.get('/videos/:nom/:code', (req, res) => {
+        var les_videos = fs.readFileSync(new URL('file:'+path.join(__dirname, 'videos.json'))); // Récupération du fichier videos.json
+        les_videos = JSON.parse(les_videos); // Transformation en chaîne de caractères
+        var good = false; // Création d'une variable permettant de savoir si il y a ou non correspondance (pour savoir quoi retourné)
+        for(i=0; i<les_videos.length; i++)
+        {
+            if(les_videos[i].nom == req.params.nom) // Si le nom correspond
+            {        
+                if(les_videos[i].code == req.params.code) // Et le code aussi
+                {
+                    good = true; // Indique que l'accès est autorisé
+                }
+            }
+        }
+        if(good) res.sendFile(path.join(__dirname, 'videos', req.params.nom+'.mp4')); // Si l'accès est autorisé renvoie la vidéo
+            else res.send("nom de vidéo ou code incorrect"); // Sinon renvoie un message d'erreur
+    }
+);
 
 app.use(express.static(path.join(__dirname, 'public'))); // Mise en place d'un dossier statique
 
